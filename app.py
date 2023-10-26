d332e8bb67538c56beddc4f7465dc8177e283a2e
Habibul Bashar Ahmed <148216438+habib-ahmed-01@users.noreply.github.com>
2023-10-26 15:53:22 +0000

    Flask Blog Setting up pages and HTML

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# 404 Page
+@app.errorhandler(404)
+def error_page(e):
+    return render_template("404.html", title="404")
+
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+# Login Page
+@app.route("/login")
+def login_page():
+    return render_template("login.html", title="Login")
