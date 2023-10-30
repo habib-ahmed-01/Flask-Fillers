ccae30bdc5d64830f077a816f075d5aedd4f0b2b
Habibul Bashar Ahmed <148216438+habib-ahmed-01@users.noreply.github.com>
2023-10-30 10:57:00 +0000

    DBsetup and Model created

--- Flask Boilerplate/website/auth.py
+++ Flask Boilerplate/website/auth.py
@@ -1,22 +1,13 @@
 from flask import Blueprint, render_template, redirect, request
-from flask_wtf import Form, StringField, Required, Email
+from flask_sqlalchemy import SQLAlchemy
 
 auth = Blueprint('auth', __name__)
-
-class ContactForm(Form):
-    username = StringField('Name', validators=[Required()])
-    email = StringField('Email', validators=[Required(), Email()])
-    password = StringField('password', validators=[Required()])
 
 # Sign Up
 @auth.route("/sign-up", methods=['GET','POST'])
 def sign_up():
     if request.method == 'POST':
-        email = request.form.get('email')
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-    print(email, username, password)
+        pass
 
     return render_template('signup.html')
 
