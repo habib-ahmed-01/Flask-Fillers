5d199e12b3b74d5399b119cda079e3b399fdedca
Habibul Bashar Ahmed <148216438+habib-ahmed-01@users.noreply.github.com>
2023-11-03 20:03:32 +0000

    OpenCv Flask

---
+++ Image Editing:OpenCv-Flask/main.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, flash, url_for
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'Image Editing:OpenCv-Flask/static/images'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def ProcessImage(filename, operation):
+    print(f'{operation}:{filename}')
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/edit", methods=['GET','POST'])
+def edit():
+    if request.method == 'POST':
+        operation = request.form.get("operation")
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return "redirect(request.url)"
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return "redirect(request.url)"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            ProcessImage(filename, operation)
+            return render_template("index.html")
+
+    return render_template("index.html")
+
+app.run(debug=True)
