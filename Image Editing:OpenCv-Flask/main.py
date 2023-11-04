8fb9e126386bf174c61ecddd25579f8dcdb7facf
Habibul Bashar Ahmed <148216438+habib-ahmed-01@users.noreply.github.com>
2023-11-04 17:53:50 +0000

    Image processing pending

--- Image Editing:OpenCv-Flask/main.py
+++ Image Editing:OpenCv-Flask/main.py
@@ -1,19 +1,41 @@
-from flask import Flask, render_template, request, flash, url_for
+from flask import Flask, render_template, request, flash, url_for, redirect
 from werkzeug.utils import secure_filename
 import os
+import cv2
 
 UPLOAD_FOLDER = 'Image Editing:OpenCv-Flask/static/images'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "Development"
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 def ProcessImage(filename, operation):
     print(f'{operation}:{filename}')
+    img = cv2.imread(f"static/images/{filename}")
+    match operation:
+        case "cgray":
+            imgprocessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(f"static/images/{filename}processed", imgprocessed)
+            return filename
+        case "cpng":
+            imgprocessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(f"static/images/{filename}processed", imgprocessed)
+            return filename
+        case "cwebp":
+            imgprocessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(f"static/images/{filename}processed", imgprocessed)
+            return filename
+        case "cjpg":
+            imgprocessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(f"static/images/{filename}processed", imgprocessed)
+            return filename
+
 
 @app.route("/")
 def home():
@@ -41,7 +63,8 @@ def edit():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             ProcessImage(filename, operation)
-            return render_template("index.html")
+            flash(f"You image has been processed and is available at <a href='/static/images/{filename}processed' target='_blank'>here</a>")
+            return redirect(url_for('home'))
 
     return render_template("index.html")
 
